7396e4888f3058a2fe748188abaf4734d8d27caf
Mario Cao <mariocao@gmail.com>
2022-04-22 17:59:14 +0200

    feat: Add basic game logic

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -7,25 +7,65 @@ contract Game {
     // Oracle price feed address
     CLV2V3Interface public priceFeed;
 
+    // Game reference price
+    int256 public referencePrice = 0;
+
+    // Game reference timestamp
+    uint256 public referenceTimestamp = 0;
+
+    // Game ended
+    bool public gameEnded = false;
+
     // Score mapping (address -> integer)
-    mapping(address => uint) scores;
+    mapping(address => uint) public scores;
 
     // List of bigger votes
-    address[] biggerVotes;
+    address[] public biggerVotes;
+
     // List of smaller votes
-    address[] smallerVotes;
+    address[] public smallerVotes;
+
+    modifier isGameActive() {
+        require(!gameEnded, "Game has ended");
+        _;
+    }
 
     constructor(address _priceFeed) {
         priceFeed = CLV2V3Interface(_priceFeed);
     }
 
-    function vote(bool isBigger) public {
-        // Store msg.sender in array
+    function start() public {
+        require(referenceTimestamp != 0, "Game was already activated");
+        (, referencePrice, , referenceTimestamp, ) = priceFeed
+            .latestRoundData();
     }
 
-    function tally() public {
-        // Reads price feed
-        // Checks timestamps
-        // If ok, assign scores to winners
+    function vote(bool isBigger) public isGameActive {
+        if (isBigger) {
+            biggerVotes.push(msg.sender);
+        } else {
+            smallerVotes.push(msg.sender);
+        }
+    }
+
+    function tally() public isGameActive {
+        (, int newPrice, , uint newTimestamp, ) = priceFeed.latestRoundData();
+
+        require(
+            newTimestamp > referenceTimestamp,
+            "Price timestamp is not valid"
+        );
+
+        if (newPrice > referencePrice) {
+            for (uint i = 0; i < biggerVotes.length; i++) {
+                scores[biggerVotes[i]]++;
+            }
+        } else if (newPrice < referencePrice) {
+            for (uint i = 0; i < smallerVotes.length; i++) {
+                scores[smallerVotes[i]]++;
+            }
+        }
+
+        gameEnded = true;
     }
 }
